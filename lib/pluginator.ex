902d36b1c00c45dc557a6a176f95e23841a5f9f0
Booker C. Bense <bbense@gmail.com>
2015-04-15 12:49:41 -0700

    Duplication of plugin code in elixgrep

--- lib/pluginator.ex
+++ lib/pluginator.ex
@@ -1,2 +1,40 @@
 defmodule Pluginator do
+  @moduledoc """
+    Utility routines for loading plugin modules. Given plugin = foo, it looks
+    in the search path for a file called foo.exs 
+  """
+
+  def load(plugin) do
+    path = search_path(plugin)
+    unless( path, do: raise "#{plugin} not found")
+    Code.require_file(path) 
+  end
+ 
+  def search_path(plugin) do
+    target = "#{plugin}.exs"
+     get_paths
+    |>
+     Enum.flat_map( fn(dir) -> entries(dir) end )
+    |>
+     Enum.filter( fn(file) -> Path.basename(file) == target end)
+    |>
+     List.first
+  end 
+
+  @doc """
+    set_path is currently a noop placeholder.
+  """
+  def set_path(path) do
+    path
+  end 
+
+  def get_paths do
+     ["./test/plugins"]
+  end 
+
+  defp entries(dir) do
+    {:ok,elist} = File.ls(dir)
+    Enum.map(elist, fn(entry) -> "#{dir}/#{entry}" end)
+  end 
+
 end
--- test/pluginator_test.exs
+++ test/pluginator_test.exs
@@ -1,7 +1,27 @@
 defmodule PluginatorTest do
-  use ExUnit.Case
+  
+ use ExUnit.Case, async: true
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  
+  test "Pluginator.get_paths returns a list of dirs" do
+    assert Pluginator.get_paths == ["./test/plugins"]
   end
+
+  test "Pluginator.search can find default test plugin" do
+    assert Pluginator.search_path("test") == "./test/plugins/test.exs"
+  end
+
+  test "Pluginator.search fails" do
+    refute Pluginator.search_path("sidehill_gouger")
+  end 
+  
+  # Does not pass for some reason. Error is raised. 
+  test "Pluginator.load raises error when plugin doesn't exist" do 
+     assert_raise RuntimeError, Pluginator.load("sidehill_gouger")
+  end
+
+  test "Pluginator.load" do
+    Pluginator.load("test")
+  end
+
 end
